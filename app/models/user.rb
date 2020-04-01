6dc8bca579bc910499026043cf07923ad40f7375
Mohamed Naser <nc_m.naser@hotmail.com>
2020-04-01 04:22:46 +0200

    add stickler and fix issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-    validates :name,  presence: true, length: { minimum: 3 ,  maximum: 150}
-    validates :email,  presence: true, length: { minimum: 3 ,  maximum: 150}
+  validates :name, presence: true, length: { minimum: 3, maximum: 150 }
+  validates :email, presence: true, length: { minimum: 3, maximum: 150 }
 
-    has_many :posts, class_name: "Post"
-    has_many :comments, class_name: "Comment"
-
+  has_many :posts, class_name: 'Post'
+  has_many :comments, class_name: 'Comment'
 end
