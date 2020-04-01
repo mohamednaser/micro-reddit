9336253bc6a4ff64fffb729b7f9f001826b3f966
Mohamed Naser <nc_m.naser@hotmail.com>
2020-04-01 04:20:26 +0200

    update comments model

--- db/migrate/20200401021308_create_comments.rb
+++ db/migrate/20200401021308_create_comments.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.user_id :integer
-      t.post_id :integer
-      t.comment :string
+      t.integer :user_id
+      t.integer :post_id
+      t.string :comment
 
       t.timestamps
     end
